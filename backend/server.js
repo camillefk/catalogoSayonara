02d88a7f7599f536c7c619cf71426d2ccb215e35
Camille Focke <contatocamillefarias@gmail.com>
2025-03-14 01:10:15 -0300

    BACKEND E MONGODB FUNCIONANDOgit add .

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config(); //para carregar as variaveis do arquivo .env (???)
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Conectado ao MongoDB'))
+  .catch((err) => console.log('Erro ao conectar ao MongoDB', err));
+
+
+const app = express();
+app.use(cors()); //comunicaçao entre front e back
+app.use(express.json()); //faz o back entender o json no corpo das requisiçoes (corpo???)
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Backend está funcionando!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
