a55871c7a4bf015d2f4d34ef7bc504198d3508e2
Camille Focke <contatocamillefarias@gmail.com>
2025-03-19 20:22:41 -0300

    component verseSection inserido na homePage

---
+++ src/components/verseSection/verseSection.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const VerseSection = () => {
+    const [verse, setVerse] = useState("");
+
+    useEffect(() => {
+        const fetchVerse = async () => {
+          try {
+            const response = await fetch("http://localhost:5000/api/verses");
+            const data = await response.json();
+            setVerse(data.text);
+          } catch (error) {
+            console.error("Erro ao buscar o versículo:", error);
+          }
+        };
+    
+        fetchVerse();
+      }, []);
+    
+      return (
+        <div className="verse-section">
+          <h2>Versículo do Dia</h2>
+          <p>{verse || "Nenhum versículo disponível no momento."}</p>
+        </div>
+      );
+    };
+    
+    export default VerseSection;
