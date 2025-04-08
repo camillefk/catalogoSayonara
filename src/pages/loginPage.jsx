955cafb447f7f4d486a1009194b6acfc7e8c0113
Camille Focke <contatocamillefarias@gmail.com>
2025-04-08 17:18:07 -0300

    Atualização no layout da tela de login

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import '../styles/loginPage.css';
 
 const LoginPage = () => {
     const [user, setUser] = useState('');
@@ -28,7 +30,8 @@ const LoginPage = () => {
 
     return (
       <div className="login-container">
-        <h2>Login</h2>
+        <img src={logo} alt="Logo da empresa" className="login-logo" />
+        <h2>Login Administrativo</h2>
         <form onSubmit={handleLogin}>
           <input
           type='text'
